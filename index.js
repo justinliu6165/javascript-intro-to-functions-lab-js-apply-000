9b7c976ebe806ed1f0bea7782931827b5f2e8f04
Justin Liu <justin_liu@hotmail.co.uk>
2018-06-12 12:44:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   if (string.toLowerCase()){
     return "I can\'t hear you!";
   }
-  if (string === string.toUpperCase()){
+  if (string.toUpperCase()){
     return "YES INDEED!";
   }
   if (string === "I love you, Grandma."){
